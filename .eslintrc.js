18e72141977330af05aa222343f7b2227c086ed6
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-02-12 21:18:35 -0500

    Configured husky, eslint, prettier, switched to yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,74 @@
 module.exports = {
-    "env": {
-        "browser": true,
-    "commonjs": true,
-        "es6": true,
-        "amd": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+    amd: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true,
-            "blockBindings": true 
-        }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/react',
+    'plugin:react/recommended',
+  ],
+  plugins: [
+    'detox',
+    'jest',
+    'react',
+    'react-native',
+    'react-hooks',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "extends": "./node_modules/blueconnect-mobile-javascript-style-guide/index.js",
-    "rules": {
-        // overrides for the front-end compile
-        'no-var': 'off',
-        'prefer-arrow-callback': 'off',
-        'prefer-destructuring': 'off',
-        'no-console': 'off'
-    },
-    "globals": {
-        "angular": true,
-        "moment": true,
-        "NativeStorage": true,
-        "$": true,
-        "Connection": true,
-        "cybersource": true,
-        "cordova": true,
-        "CryptoJS": true,
-        "FingerprintAuth": true,
-        "device": true
-    }
+  },
+  rules: {
+    // overrides for the front-end compile
+    'no-var': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-cycle': 'warn',
+    'react/prop-types': 0,
+    'prefer-arrow-callback': 'warn',
+    'prefer-destructuring': 'warn',
+    'no-console': 'warn',
+    'no-plusplus': 'warn',
+    'no-underscore-dangle': 'off',
+    'new-cap': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'none',
+      },
+    ],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-direct-mutation-state': 'warn',
+    'no-use-before-define': 'warn',
+    'no-param-reassign': 'warn',
+    'consistent-return': 'warn',
+    'no-nested-ternary': 'warn',
+    'no-return-assign': 'warn',
+    'global-require': 'warn',
+    'import/no-cycle': 'warn',
+    'max-classes-per-file': 'warn',
+    camelcase: 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
