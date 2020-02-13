d8de79590e2031c765af455ad8b310448291bec5
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-02-12 22:21:58 -0500

    Converted Atom to pure functionality component

--- src/components/Atom.js
+++ src/components/Atom.js
@@ -1,11 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-class Atom extends React.Component {
-    render() {
-        return (
-            <div className={`atom ${this.props.name} a-${this.props.index}`}></div>
-        );
-    }
-}
-
-export default Atom;
+const Atom = () => (
+  <div className={`atom ${this.props.name} a-${this.props.index}`}></div>
+)
+export default Atom
